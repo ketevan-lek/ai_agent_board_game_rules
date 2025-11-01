eecd3a96111d8634c9c7679fb3b6fe761692710b
mar-sv <masv@dhigroup.com>
2025-11-01 16:10:49 +0100

    implemented google search for rules

--- web_crawler.py
+++ web_crawler.py
@@ -7,6 +7,7 @@
 import requests
 from dotenv import load_dotenv
 import os
+from pathlib import Path
 load_dotenv()
 
 
@@ -23,10 +24,32 @@ def query_google(board_game):
         "num": 5,
     }
 
-    r = requests.get(url, params=params)
+    try:
+        r = requests.get(url, params=params)
+    except Exception as e:
+        raise (f"{e}")
+
     results = r.json().get("items", [])
 
     for i, item in enumerate(results, 1):
-        print(f"{i}. {item['title']}")
-        print(item['link'])
-        print(item['snippet'], "\n")
+        url = item["link"]
+        if url.split(".")[-1] == "pdf":
+            save_pdf(url)
+            break
+
+
+def save_pdf(url, save_dir="pdfs"):
+    if isinstance(save_dir, str):
+        save_dir = Path(save_dir)
+    save_dir.mkdir(parents=True, exist_ok=True)
+
+    if url.lower().endswith(".pdf"):
+        filename = save_dir.joinpath(os.path.basename(url.split("?")[0]))
+        try:
+            r = requests.get(url, timeout=15)
+            r.raise_for_status()
+            with open(filename, "wb") as f:
+                f.write(r.content)
+            print(f"✅ Downloaded {filename}")
+        except Exception as e:
+            print(f"⚠️ Failed: {url} ({e})")
