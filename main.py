8f57800a1eb69f1d4255e711c3b6804e786964fb
mar-sv <masv@dhigroup.com>
2025-11-01 16:42:17 +0100

    start_with_graph

--- main.py
+++ main.py
@@ -6,8 +6,36 @@ from langchain.chat_models import init_chat_model
 from typing_extensions import TypedDict
 from pydantic import BaseModel, Field
 
+from web_crawler import query_google
+
 load_env = load_dotenv()
+
+llm = init_chat_model("gpt-4o-mini")
 
 
 class State(TypedDict):
-    messages: Annotated[List, add_messages]
+    # messages: Annotated[List, add_messages]
+    url_basename: str | None
+    game_name: str | None
+
+
+def google_search(state):
+    # query_google()
+    # return
+    pass
+
+
+def analyze_pdf(state):
+    # return
+    pass
+
+
+graph_builder = StateGraph(State)
+
+graph_builder.add_node("google_search", google_search)
+graph_builder.add_node("analyze_pdf", analyze_pdf)
+
+graph_builder.add_edge(START, start_key="google_search")
+graph_builder.add_edge(start_key="google_search", end_key="analyze_pdf")
+
+graph_builder.add_edge(start_key="analyze_pdf", end_key=END)
