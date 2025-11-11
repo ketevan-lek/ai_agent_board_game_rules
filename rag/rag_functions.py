714058d0d86812a7313d85d606af558215e344bc
mar-sv <masv@dhigroup.com>
2025-11-11 16:44:10 +0100

    add base for rag

---
+++ rag/rag_functions.py
@@ -0,0 +1,21 @@
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.prompts import MessagesPlaceholder
+from langchain_core.prompts import ChatPromptTemplate
+
+
+def extend_chathistory(chat_history, question, response):
+    chat_history.extend([HumanMessage(content=question)],
+                        AIMessage(content=response))
+    return chat_history
+
+
+def create_context_q_prompt():
+
+    sys_prompt = """
+        Given a chat history and a user question, answer the question with the previous context. 
+        """
+
+    return ChatPromptTemplate.from_messages([("system", sys_prompt),
+                                             (MessagesPlaceholder(
+                                                 "chat_history")),
+                                             ("human", "{input}")])
